6047af2c04597838a39da23710d07b481ad8ce3b
Latmi <sbezuglii@mail.ru>
2018-08-17 15:07:32 +0300

    install mogodb

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+const arr = ["hello", "world", "test"];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.render("index", { arr: arr });
+});
+app.get("/create", function(req, res) {
+  res.render("create");
+});
+
+app.post("/create", (req, res) => {
+  arr.push(req.body.text);
+  res.redirect("/");
 });
 
 app.listen(3000);
